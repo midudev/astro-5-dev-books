3ceba0b83d13ebd5a83f26503cf5801e9592e6b9
Miguel Ángel Durán <miduga@gmail.com>
2024-09-18 19:36:43 +0200

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
+
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
       SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
     }
-  }
+  },
+
+  adapter: vercel()
 });
